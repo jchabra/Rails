81733602b4b4413e5a1d9d450b850de648bf77c4
Jasmine Chabra <jchabra@gmail.com>
2012-11-12 12:32:32 -0500

    stuff

---
+++ labs/beginner/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beginner::Application.config.secret_token = '7a3d18b6b85c96ec9b57ab5404f737412fa78614fa6ed8e11632a2bdc2af6039fd0e2859c1e8c8b4a66122d5e6a3c298869afccc4ae5ebe766eb9d058c6a9376'
