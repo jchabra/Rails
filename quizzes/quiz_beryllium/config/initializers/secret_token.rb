1458755265f2632eba033e6f0263b5cd0a77ffb8
Jasmine Chabra <jchabra@gmail.com>
2012-11-29 12:24:12 -0500

    more quizzes

---
+++ quizzes/quiz_beryllium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizBeryllium::Application.config.secret_token = 'c495af85fdf02ff0e10f276cdcac0f46d91844d54c27edf963d5fdee3feea141ea9b53a46c8bd17a7af983d39c068faf9a444e6e283e4dbd716e760ac33fcdb0'
